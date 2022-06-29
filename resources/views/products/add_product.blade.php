1cbf840e751ada77b47530762ff68d9fb77e03f8
Ochieng Paul <ochieng442@outlook.com>
2022-06-29 14:12:24 +0300

    new change

--- resources/views/products/add_product.blade.php
+++ resources/views/products/add_product.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Breadcrumb -->
-<nav class="flex py-3 px-5 text-gray-700 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+<nav class="flex mb-16 py-3 px-5 text-gray-700 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700"
 aria-label="Breadcrumb">
 <ol class="inline-flex items-center space-x-1 md:space-x-3">
     <li class="inline-flex items-center">
@@ -30,13 +30,102 @@ aria-label="Breadcrumb">
     </li>
 </ol>
 </nav>
-<div class="grid lg:grid-cols-3 gap-6 mx-auto container">
-    <div class="w-full col-span-1 bg-black">
-        heel
+<div class=" mx-auto container">
+    <form class="grid xl:grid-cols-5 gap-2">
+    <div class="w-full col-span-2 space-y-6">
+
+    <div class="">
+        <label for="productname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Name</label>
+        <input type="text" id="productname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="">
     </div>
-    <div class="w-full col-span-2 bg-green-500">
-        sip
+    <div class="grid gap-6  lg:grid-cols-3">
+        <div class="w-full lg:col-span-2">
+            <label for="Category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Category</label>
+            <input type="text" id="Category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="">
+        </div>
+        <div class="lg:col-span-1">
+            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Gender</label>
+            <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected="">Choose a Gender</option>
+              <option value="US">Male</option>
+              <option value="CA">Female</option>
+              <option value="CA">For all</option>
+              <option value="FR">Others</option>
+            </select>
+        </div>
+
     </div>
+    <div>
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Product Duration</label>
+        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option selected="">Choose a Duration</option>
+          <option value="US">New</option>
+          <option value="CA">Old</option>
+          <option value="CA">Used</option>
+          <option value="FR">Others</option>
+        </select>
+    </div>
+    <div class="">
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Brand</label>
+        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option selected="">Choose a Brand</option>
+          <option value="US">Dell</option>
+          <option value="CA">Apple</option>
+          <option value="FR">Samsung</option>
+          <option value="DE">Nokia</option>
+        </select>
+    </div>
+    <div class="">
+        <label for="Location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Pickup Location</label>
+        <input type="text" id="Location" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="">
+    </div>
+    <div>
+        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Description</label>
+        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your description..."></textarea>
+    </div>
+
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </div>
+    <div class="w-full col-span-3">
+        <div class="w-full lg:grid grid-cols-6">
+            <div class="w-full flex space-x-5 col-span-4">
+                <img src="{{ asset('img/chair.jpg') }}"  width="30%"   height="30%" alt="">
+                <div class="flex justify-center items-center w-4/12">
+                    <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div class="flex flex-col justify-center items-center pt-5 pb-6">
+                            <svg class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" class="hidden">
+                    </label>
+                </div>
+            </div>
+            <div class="w-full col-span-2">
+                <div class="flex justify-center items-center w-4/12">
+                    <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div class="flex flex-col justify-center items-center pt-5 pb-6">
+                            <svg class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" class="hidden">
+                    </label>
+                </div>
+                <div class="flex justify-center items-center w-4/12">
+                    <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                        <div class="flex flex-col justify-center items-center pt-5 pb-6">
+                            <svg class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                        </div>
+                        <input id="dropzone-file" type="file" class="hidden">
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 </div>
 <div style="height: 100px;width:100%;"></div>
 @endsection
