fed53a2180fa2d4d1d9575b679f678bc3b66c639
Ochieng Paul <ochieng442@outlook.com>
2022-06-29 14:31:06 +0300

    link change

--- resources/views/Pages/Home.blade.php
+++ resources/views/Pages/Home.blade.php
@@ -192,8 +192,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   <div class="flex justify-center reveal">
@@ -207,8 +208,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   <div class="flex justify-center reveal">
@@ -222,8 +224,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   <div class="flex justify-center reveal">
@@ -237,8 +240,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   <div class="flex justify-center reveal">
@@ -252,8 +256,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   <div class="flex justify-center reveal">
@@ -267,8 +272,9 @@
                           Some quick example text to build on the card title and make up the bulk of the card's
                           content.
                         </p>
-                        <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
-                      </div>
+ <a href="{{route('Products')}}">
+                            <button type="button" class=" inline-block px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">Trade Now</button>
+                        </a>                      </div>
                     </div>
                   </div>
                   {{-- bottom --}}
